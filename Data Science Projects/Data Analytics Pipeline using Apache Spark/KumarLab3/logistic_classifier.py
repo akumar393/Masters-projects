3b664e6e6bf90ad8560d4389c9b46e7edb14e372
ABHINAV <31447285+ABHINAVKO@users.noreply.github.com>
2018-05-24 18:46:37 -0500

    Latest data science project

---
+++ Data Science Projects/Data Analytics Pipeline using Apache Spark/KumarLab3/logistic_classifier.py
@@ -0,0 +1,155 @@
+from pyspark.sql import SQLContext
+from pyspark import SparkContext
+from pyspark.ml.feature import RegexTokenizer, StopWordsRemover, CountVectorizer
+from pyspark.ml.classification import LogisticRegression
+from pyspark.ml import Pipeline
+from pyspark.ml.feature import OneHotEncoder, StringIndexer, VectorAssembler
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+from pyspark.ml.classification import RandomForestClassifier
+from pyspark.sql.functions import col
+
+
+sc =SparkContext()
+sqlContext = SQLContext(sc)
+df = sqlContext.read.format("csv").options(header='true', inferschema='true').load('/home/hadoop/host/Categories/train4.csv')
+columns=("id","comment_text")
+df=df.select(*columns)
+df=df[(df.id==1) | (df.id==2) | (df.id==3) | (df.id==4)]
+df=df.orderBy(col("id").asc()).dropna(subset=('id'))
+df.show(5)
+'''
+from pyspark.sql.functions import col
+df.groupBy("id") \
+    .count() \
+    .orderBy(col("count").desc()) \
+	.dropna(subset=('id')) \
+    .show()
+
+df.groupBy("comment_text") \
+    .count() \
+    .orderBy(col("count").desc()) \
+	.dropna(subset=('comment_text')) \
+    .show()
+'''
+# regular expression tokenizer
+regexTokenizer = RegexTokenizer(inputCol="comment_text", outputCol="words", pattern="\\W")
+# stop words
+stopwords = ['a', 'about', 'above', 'across', 'after', 'afterwards']
+stopwords += ['again', 'against', 'all', 'almost', 'alone', 'along']
+stopwords += ['already', 'also', 'although', 'always', 'am', 'among']
+stopwords += ['amongst', 'amoungst', 'amount', 'an', 'and', 'another']
+stopwords += ['any', 'anyhow', 'anyone', 'anything', 'anyway', 'anywhere']
+stopwords += ['are', 'around', 'as', 'at', 'back', 'be', 'became']
+stopwords += ['because', 'become', 'becomes', 'becoming', 'been']
+stopwords += ['before', 'beforehand', 'behind', 'being', 'below']
+stopwords += ['beside', 'besides', 'between', 'beyond', 'bill', 'both']
+stopwords += ['bottom', 'but', 'by', 'call', 'can', 'cannot', 'cant']
+stopwords += ['co', 'computer', 'con', 'could', 'couldnt', 'cry', 'de']
+stopwords += ['describe', 'detail', 'did', 'do', 'done', 'down', 'due']
+stopwords += ['during', 'each', 'eg', 'eight', 'either', 'eleven', 'else']
+stopwords += ['elsewhere', 'empty', 'enough', 'etc', 'even', 'ever']
+stopwords += ['every', 'everyone', 'everything', 'everywhere', 'except']
+stopwords += ['few', 'fifteen', 'fifty', 'fill', 'find', 'fire', 'first']
+stopwords += ['five', 'for', 'former', 'formerly', 'forty', 'found']
+stopwords += ['four', 'from', 'front', 'full', 'further', 'get', 'give']
+stopwords += ['go', 'had', 'has', 'hasnt', 'have', 'he', 'hence', 'her']
+stopwords += ['here', 'hereafter', 'hereby', 'herein', 'hereupon', 'hers']
+stopwords += ['herself', 'him', 'himself', 'his', 'how', 'however']
+stopwords += ['hundred', 'i', 'ie', 'if', 'in', 'inc', 'indeed']
+stopwords += ['interest', 'into', 'is', 'it', 'its', 'itself', 'keep']
+stopwords += ['last', 'latter', 'latterly', 'least', 'less', 'ltd', 'made']
+stopwords += ['many', 'may', 'me', 'meanwhile', 'might', 'mill', 'mine']
+stopwords += ['more', 'moreover', 'most', 'mostly', 'move', 'much']
+stopwords += ['must', 'my', 'myself', 'name', 'namely', 'neither', 'never']
+stopwords += ['nevertheless', 'next', 'nine', 'no', 'nobody', 'none']
+stopwords += ['noone', 'nor', 'not', 'nothing', 'now', 'nowhere', 'of']
+stopwords += ['off', 'often', 'on','once', 'one', 'only', 'onto', 'or']
+stopwords += ['other', 'others', 'otherwise', 'our', 'ours', 'ourselves']
+stopwords += ['out', 'over', 'own', 'part', 'per', 'perhaps', 'please']
+stopwords += ['put', 'rather', 're', 's', 'same', 'see', 'seem', 'seemed']
+stopwords += ['seeming', 'seems', 'serious', 'several', 'she', 'should']
+stopwords += ['show', 'side', 'since', 'sincere', 'six', 'sixty', 'so']
+stopwords += ['some', 'somehow', 'someone', 'something', 'sometime']
+stopwords += ['sometimes', 'somewhere', 'still', 'such', 'system', 'take']
+stopwords += ['ten', 'than', 'that', 'the', 'their', 'them', 'themselves']
+stopwords += ['then', 'thence', 'there', 'thereafter', 'thereby']
+stopwords += ['therefore', 'therein', 'thereupon', 'these', 'they']
+stopwords += ['thick', 'thin', 'third', 'this', 'those', 'though', 'three']
+stopwords += ['three', 'through', 'throughout', 'thru', 'thus', 'to']
+stopwords += ['together', 'too', 'top', 'toward', 'towards', 'twelve']
+stopwords += ['twenty', 'two', 'un', 'under', 'until', 'up', 'upon']
+stopwords += ['us', 'very', 'via', 'was', 'we', 'well', 'were', 'what']
+stopwords += ['whatever', 'when', 'whence', 'whenever', 'where']
+stopwords += ['whereafter', 'whereas', 'whereby', 'wherein', 'whereupon']
+stopwords += ['wherever', 'whether', 'which', 'while', 'whither', 'who']
+stopwords += ['whoever', 'whole', 'whom', 'whose', 'why', 'will', 'with']
+stopwords += ['within', 'without', 'would', 'yet', 'you', 'your']
+stopwords += ['yours', 'yourself', 'yourselves', 'https', '//t', '&amp;']
+
+stopwordsRemover = StopWordsRemover(inputCol="words", outputCol="filtered").setStopWords(stopwords)
+# bag of words count
+countVectors = CountVectorizer(inputCol="filtered", outputCol="features", vocabSize=10000, minDF=5)
+
+
+label_stringIdx = StringIndexer(inputCol = "id", outputCol = "label")
+pipeline = Pipeline(stages=[regexTokenizer, stopwordsRemover, countVectors, label_stringIdx])
+# Fit the pipeline to training documents.
+pipelineFit = pipeline.fit(df)
+dataset = pipelineFit.transform(df)
+dataset.show(5)
+
+# set seed for reproducibility
+(trainingData, testData) = dataset.randomSplit([0.7, 0.3], seed = 100)
+print("Training Dataset Count: " + str(trainingData.count()))
+print("Test Dataset Count: " + str(testData.count()))
+
+
+lr = LogisticRegression(maxIter=20, regParam=0.3, elasticNetParam=0)
+lrModel = lr.fit(trainingData)
+predictions = lrModel.transform(testData)
+predictions.filter(predictions['prediction'] == 0) \
+    .select("comment_text","id","probability","label","prediction") \
+    .orderBy("probability", ascending=False) \
+    .show(n = 10, truncate = 30)
+	
+evaluator = MulticlassClassificationEvaluator(predictionCol="prediction")
+accuracy=evaluator.evaluate(predictions)
+print("Test set accuracy=",accuracy)
+
+
+
+'''
+from pyspark.ml.classification import NaiveBayes
+nb = NaiveBayes(smoothing=1)
+model = nb.fit(trainingData)
+predictions = model.transform(testData)
+predictions.filter(predictions['prediction'] == 0) \
+    .select("comment_text","id","probability","label","prediction") \
+    .orderBy("probability", ascending=False) \
+    .show(n = 10, truncate = 30)
+	
+	
+evaluator = MulticlassClassificationEvaluator(predictionCol="prediction")
+evaluator.evaluate(predictions)
+accuracy=evaluator.evaluate(predictions)
+print("Test set accuracy=",accuracy)
+
+rf = RandomForestClassifier(labelCol="label", \
+                            featuresCol="features", \
+                            numTrees = 100, \
+                            maxDepth = 4, \
+                            maxBins = 32)
+# Train model with Training Data
+rfModel = rf.fit(trainingData)
+predictions = rfModel.transform(testData)
+predictions.filter(predictions['prediction'] == 0) \
+    .select("comment_text","id","probability","label","prediction") \
+    .orderBy("probability", ascending=False) \
+    .show(n = 10, truncate = 30)
+	
+evaluator = MulticlassClassificationEvaluator(predictionCol="prediction")
+evaluator.evaluate(predictions)
+accuracy=evaluator.evaluate(predictions)
+print("Test set accuracy=",accuracy)
+
+'''
